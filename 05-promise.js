f4fdfe6e28a4cdddfdda2956bfffb5ffe13de5fc
PC-03 <johannarojasgon@gmail.com>
2017-07-17 14:51:09 +0200

    initial commit async callback

--- 05-promise.js
+++ 05-promise.js
@@ -15,3 +15,24 @@ const logsSync = () => {
 console.time('logsSync()');
 logsSync();
 console.timeEnd('logsSync()');
+
+const  logAsync = (logPath, msg, cb) => {
+  fs.appendFile(logPath, msg + '\n', cb);
+};
+
+const logsAsync = () => {
+  logAsync('logs/app.log', 'Ligne 1', () => {
+    logAsync('logs/app.log', 'Ligne 2', () => {
+      logAsync('logs/app.log', 'Ligne 3', () => {
+        logAsync('logs/app.log', 'Ligne 4', () => {
+          logAsync('logs/app.log', 'Ligne 5', () => {
+          });
+        });
+      });
+    });
+  });
+}
+
+console.time('logsAsync()');
+logsAsync();
+console.timeEnd('logsAsync()');
